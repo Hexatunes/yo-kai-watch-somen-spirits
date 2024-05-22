659cb50fc4aa02e49932f9a1c7862f7aa47359e9
Hexatunes <63250225+Hexatunes@users.noreply.github.com>
2024-05-22 19:52:06 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+function actuallySwitchTeambuilder(){
+  location.href = "/teambuilder.html";
+}
+
+function intoFinished(){
+  document.getElementById("padTrans").style.display = "none";
+}
+
+function switchTeambuilder(){
+  document.getElementById("padTrans").style.display = "block";
+  document.getElementById("padTrans").style.animation = "fadeIn 1s";
+  setTimeout(actuallySwitchTeambuilder, 1000)
+}
+
+const songs = ["Harrisville.mp3", "Springdale.mp3", "Blossom.mp3"]
+
+function startGame(){
+  document.getElementById("startMenu").style.display = "none";
+  const randomSong = Math.floor(Math.random() * songs.length);
+  const path = "/audios/music/" + songs[randomSong];
+  document.getElementById("bgm").src = path;
+  document.getElementById("bgm").play()
+  document.getElementById("bgm").volume = 0.5
+}
+
+var musicPlaying = true;
+
+function toggleMusic(){
+  if(musicPlaying){
+    document.getElementById("toggleMusic").src = "/images/musicOFF.png";
+    document.getElementById("bgm").volume = 0.0;
+    musicPlaying = false;
+  }else{
+    document.getElementById("toggleMusic").src = "/images/musicON.png";
+    document.getElementById("bgm").volume = 0.5;
+    musicPlaying = true;
+  }
+}
+
+function hidePad(){
+  document.getElementById("padTrans").style.animation = "fadeOut 1s";
+  setTimeout(intoFinished, 1000)
+}
---
+++ scripts/teambuilder.js
@@ -0,0 +1,26 @@
+function actuallySwitchHome(){
+  location.href = "/";
+}
+
+function intoFinished(){
+  document.getElementById("padTrans").style.display = "none";
+}
+
+function switchHome(){
+  document.getElementById("padTrans").style.display = "block";
+  document.getElementById("padTrans").style.animation = "fadeIn 1s";
+  setTimeout(actuallySwitchHome, 1000)
+}
+
+const songs = ["Harrisville.mp3", "Springdale.mp3", "Blossom.mp3"]
+
+function setUp(){
+  const randomSong = Math.floor(Math.random() * songs.length);
+  const path = "/audios/music/" + songs[randomSong];
+  document.getElementById("bgm").src = path;
+  document.getElementById("bgm").play()
+  document.getElementById("bgm").volume = 0.5
+  
+  document.getElementById("padTrans").style.animation = "fadeOut 1s";
+  setTimeout(intoFinished, 1000)
+}
